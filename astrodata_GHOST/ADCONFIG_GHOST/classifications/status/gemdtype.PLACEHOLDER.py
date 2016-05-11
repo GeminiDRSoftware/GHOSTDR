de30359ce433c611cf9d757263bbc5a45742f1fe
Marc White <marc.white@anu.edu.au>
2016-05-11 11:18:34 +1000

    Fixed typo in template type definition

--- astrodata_GHOST/ADCONFIG_GHOST/classifications/status/gemdtype.PLACEHOLDER.py
+++ astrodata_GHOST/ADCONFIG_GHOST/classifications/status/gemdtype.PLACEHOLDER.py
@@ -2,7 +2,7 @@ class PLACEHOLDER(DataClassification):
 
     name = "PLACEHOLDER"
     usage = 'Applies to all data that has been processed "this way"'
-    parent = 'UNPREPARED;
+    parent = 'UNPREPARED'
     requirement = PHU({'{re}.*?SOMEHDR*?': ".*?" })
 
 newtypes.append(PLACEHOLDER)
