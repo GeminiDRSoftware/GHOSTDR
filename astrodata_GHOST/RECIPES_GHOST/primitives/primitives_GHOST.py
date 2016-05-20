d42bffe7483ee0942804e730721a882aa4a9e82c
Lance Luvaul <lance.luvaul@anu.edu.au>
2016-05-20 16:38:45 +1000

    begin fleshing out the makeProcessedBias.GHOST recipe which, for now, only includes adding the GHOST_BIAS type and the validateData primitive

--- astrodata_GHOST/RECIPES_GHOST/primitives/primitives_GHOST.py
+++ astrodata_GHOST/RECIPES_GHOST/primitives/primitives_GHOST.py
@@ -1,4 +1,5 @@
 from astrodata import AstroData
+from astrodata.utils import logutils
 from gempy.gemini import gemini_tools as gt
 
 from primitives_GEMINI import GEMINIPrimitives
@@ -15,6 +16,66 @@ class GHOSTPrimitives(GEMINIPrimitives):
     def init(self, rc):
         GEMINIPrimitives.init(self, rc)
         return rc
+
+    def validateData(self, rc):
+        """
+        This primitive is used to validate GHOST data, specifically.
+        """
+
+        # Instantiate the log
+        log = logutils.get_logger(__name__)
+
+        # Log the standard "starting primitive" debug message
+        log.debug(gt.log_message("primitive", "validateData", "starting"))
+
+        # Define the keyword to be used for the time stamp for this primitive
+        timestamp_key = self.timestamp_keys["validateData"]
+
+        # Initialize the list of output AstroData objects
+        adoutput_list = []
+
+        # Loop over each input AstroData object in the input list
+        for ad in rc.get_inputs_as_astrodata():
+
+            # Check whether the validateData primitive has been run previously
+            if ad.phu_get_key_value(timestamp_key):
+                log.warning("No changes will be made to %s, since it has "
+                            "already been processed by validateData"
+                            % ad.filename)
+
+                # Append the input AstroData object to the list of output
+                # AstroData objects without further processing
+                adoutput_list.append(ad)
+                continue
+
+            # Validate the input AstroData object by ensuring that it has
+            # 4 science extensions (NOTE: this might not be correct for
+            # spectroscopy)
+            num_ext = ad.count_exts("SCI")
+            if num_ext != 4:
+                raise Errors.Error("The number of extensions in %s do "
+                                   "match with the number of extensions "
+                                   "expected in raw GHOST data."
+                                   % ad.filename)
+            else:
+                log.fullinfo("The GHOST input file has been validated: %s "
+                             "contains %d extensions" % (ad.filename, num_ext))
+
+            # Add the appropriate time stamps to the PHU
+            gt.mark_history(adinput=ad, primname=self.myself(), keyword=timestamp_key)
+
+            # Change the filename
+            ad.filename = gt.filename_updater(adinput=ad, suffix=rc["suffix"],
+                                              strip=True)
+
+            # Append the output AstroData object to the list of output
+            # AstroData objects
+            adoutput_list.append(ad)
+
+        # Report the list of output AstroData objects to the reduction context
+        rc.report_output(adoutput_list)
+
+        yield rc
 
     def myScienceStep(self, rc):
         """
