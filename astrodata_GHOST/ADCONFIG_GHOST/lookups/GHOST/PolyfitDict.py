8f53f49be893e1458902d7f26aaf9f8fa0599393
Marc White <marc.white@anu.edu.au>
2016-12-15 14:46:33 +1100

    Values inserted into PolyfitDict

--- astrodata_GHOST/ADCONFIG_GHOST/lookups/GHOST/PolyfitDict.py
+++ astrodata_GHOST/ADCONFIG_GHOST/lookups/GHOST/PolyfitDict.py
@@ -1,6 +1,13 @@
 # This is a dictionary containing the assignments of initial Polyfit models
 # to various GHOST frame types
 
+# Keys are of the format
+# instrument_detectorXBin_detectorYBin_Arm_ResMode
+
 polyfit_dict = {
-    # 'key': 'filename',
+    # 'key': 'relative_dir',
+    'GHOST_1_1_red_std': 'GHOST/Polyfit/red/161120/std/',
+    'GHOST_1_1_red_high': 'GHOST/Polyfit/red/161120/high/',
+    'GHOST_1_1_blue_std': 'GHOST/Polyfit/blue/161120/std/',
+    'GHOST_1_1_blue_high': 'GHOST/Polyfit/blue/161120/high/',
 }
