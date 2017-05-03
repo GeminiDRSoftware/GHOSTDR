e67974f5149879136a8df524b421ba30ab09d84d
Joao Bento <joao.bento@anu.edu.au>
2017-05-03 17:07:14 +1000

    Reduction script is much nicer now. Further testing and modifications needed.

--- utils/reduce_all.sh
+++ utils/reduce_all.sh
@@ -1,71 +1,60 @@
-#!/usr/bin/bash
+#!/bin/sh
 
 #Script to reduce all GHOST data using the recipe system.
 #This must be ran from within where the GHOST data are kept, with default variables indicating where the various types of files are
 
-#$DIR = `pwd`
+# Start by setting up all the locations for the files. Change this for each case. 
+COREDIR=`pwd`
 
-#rm -Rf calibrations/
-#
-# echo 'Doing slits now'
-# typewalk --types GHOST_SLITV_BIAS --dir biases/ -o bias.list
-# reduce @bias.list
-# typewalk --types GHOST_SLITV_DARK --dir darks/ -o dark.list
-# reduce @dark.list --override_cal processed_bias:`ls calibrations/storedcals/bias*SLIT*.fits`
+CALDIR=$COREDIR'/calibrations/storedcals'
 
-# #FLATS
-# typewalk --types GHOST_SLITV_FLAT GHOST_SLITV_HIGH --dir flats/high/ -o flat.list
-# reduce @flat.list --override_cal processed_bias:`ls calibrations/storedcals/bias*SLIT*.fits` processed_dark:`ls calibrations/storedcals/dark*SLIT*.fits`
-# typewalk --types GHOST_SLITV_FLAT --dir flats/std/ -o flat.list
-# reduce @flat.list --override_cal processed_bias:`ls calibrations/storedcals/bias*SLIT*.fits` processed_dark:`ls calibrations/storedcals/dark*SLIT*.fits`
-
-# #ARCS
-# typewalk --types GHOST_SLITV_ARC GHOST_SLITV_HIGH --dir arcs/ -o arc.list
-# reduce @arc.list --override_cal processed_bias:`ls calibrations/storedcals/bias*SLIT*.fits` processed_dark:`ls calibrations/storedcals/dark*SLIT*.fits` processed_slitflat:`ls calibrations/storedcals/flat*high*SLIT*.fits` 
-# typewalk --types GHOST_SLITV_ARC GHOST_SLITV_STD --dir arcs/ -o arc.list
-# reduce @arc.list --override_cal processed_bias:`ls calibrations/storedcals/bias*SLIT*.fits` processed_dark:`ls calibrations/storedcals/dark*SLIT*.fits` processed_slitflat:`ls calibrations/storedcals/flat*std*SLIT*.fits`
+BIASDIR=$COREDIR'/biases'
+DARKDIR=$COREDIR'/darks'
+#Assumes the high and std flats are inside high/ and std/ dirs in FLATDIR
+FLATDIR=$COREDIR'/flats'
+ARCDIR=$COREDIR'/arcs'
 
 
+echo 'Doing slits now'
+typewalk --types GHOST_SLITV_BIAS --dir $BIASDIR/ -o bias.list
+reduce @bias.list
+typewalk --types GHOST_SLITV_DARK --dir $DARKDIR/ -o dark.list
+reduce @dark.list --override_cal processed_bias:`ls $CALDIR/bias*SLIT*.fits`
 
-# echo 'Now the spectrograph data'
-# echo 'Starting with the blue arm'
-# typewalk --types GHOST_BIAS GHOST_BLUE --dir biases/ -o bias.list
-# reduce @bias.list
-# typewalk --types GHOST_DARK GHOST_BLUE --dir darks/ -o dark.list
-# reduce @dark.list --override_cal processed_bias:`ls calibrations/storedcals/bias_*_blue_*.fits`
-
-# #FLATS
-# typewalk --types GHOST_FLAT GHOST_BLUE GHOST_HIGH --dir flats/high/ -o flat.list
-# reduce @flat.list --override_cal processed_bias:`ls calibrations/storedcals/bias*blue_*.fits` processed_dark:`ls calibrations/storedcals/dark*blue*.fits`
-# typewalk --types GHOST_FLAT GHOST_BLUE GHOST_STD --dir flats/std/ -o flat.list
-# reduce @flat.list --override_cal processed_bias:`ls calibrations/storedcals/bias*blue_*.fits` processed_dark:`ls calibrations/storedcals/dark*blue*.fits`
+#FLATS
+typewalk --types GHOST_SLITV_FLAT GHOST_SLITV_HIGH --dir $FLATDIR/high/ -o flat.list
+reduce @flat.list --override_cal processed_bias:`ls $CALDIR/bias*SLIT*.fits` processed_dark:`ls $CALDIR/dark*SLIT*.fits`
+# TODO: Note that GHOST_SLITV_STD is not a thing, so this needs fixing.
+typewalk --types GHOST_SLITV_FLAT --dir $FLATDIR/std/ -o flat.list
+reduce @flat.list --override_cal processed_bias:`ls $CALDIR/bias*SLIT*.fits` processed_dark:`ls $CALDIR/dark*SLIT*.fits`
 
 #ARCS
-typewalk --types GHOST_ARC GHOST_BLUE GHOST_HIGH --dir arcs/ -o arc.list
-reduce @arc.list --override_cal processed_bias:`ls calibrations/storedcals/bias*blue_*.fits` processed_dark:`ls calibrations/storedcals/dark*blue*.fits` processed_slitflat:`ls calibrations/storedcals/flat*high*SLIT*.fits` processed_slit:`ls calibrations/storedcals/arc*high*SLIT*.fits` processed_polyfit:`ls calibrations/storedcals/*blue*high*xmod*.fits`
-typewalk --types GHOST_ARC GHOST_BLUE GHOST_STD --dir arcs/ -o arc.list
-reduce @arc.list --override_cal processed_bias:`ls calibrations/storedcals/bias*blue_*.fits` processed_dark:`ls calibrations/storedcals/dark*blue*.fits` processed_slitflat:`ls calibrations/storedcals/flat*high*SLIT*.fits` processed_slit:`ls calibrations/storedcals/arc*std*SLIT*.fits`  processed_polyfit:`ls calibrations/storedcals/*blue*std*xmod*.fits`
+typewalk --types GHOST_SLITV_ARC GHOST_SLITV_HIGH --dir $ARCDIR/ -o arc.list
+reduce @arc.list --override_cal processed_bias:`ls $CALDIR/bias*SLIT*.fits` processed_dark:`ls $CALDIR/dark*SLIT*.fits` processed_slitflat:`ls $CALDIR/flat*high*SLIT*.fits` 
+typewalk --types GHOST_SLITV_ARC GHOST_SLITV_STD --dir $ARCDIR/ -o arc.list
+reduce @arc.list --override_cal processed_bias:`ls $CALDIR/bias*SLIT*.fits` processed_dark:`ls $CALDIR/dark*SLIT*.fits` processed_slitflat:`ls $CALDIR/flat*std*SLIT*.fits`
 
 
+echo 'Now the spectrograph data'
+for i in blue red 
+do
+    echo "Doing $i images now"
+    CAPCAM=`echo $i | tr '[:lower:]' '[:upper:]'`
+    typewalk --types GHOST_BIAS GHOST_$CAPCAM --dir "$BIASDIR" -o bias.list
+    reduce @bias.list
+    typewalk --types GHOST_DARK GHOST_$CAPCAM --dir $DARKDIR -o dark.list
+    reduce @dark.list --override_cal processed_bias:`ls $CALDIR/bias*$i*.fits`
+    for j in high std
+    do
+	CAPMODE=`echo $j | tr '[:lower:]' '[:upper:]'`
+	typewalk --types GHOST_FLAT GHOST_$CAPCAM GHOST_$CAPMODE --dir $FLATDIR/$j/ -o flat.list
+	reduce @flat.list --override_cal processed_bias:`ls $CALDIR/bias*$i*.fits` processed_dark:`ls $CALDIR/dark*$i*.fits`
+	typewalk --types GHOST_ARC GHOST_$CAPCAM GHOST_$CAPMODE --dir $ARCDIR/ -o arc.list
+	reduce @arc.list --override_cal processed_bias:`ls $CALDIR/bias*$i*.fits` processed_dark:`ls $CALDIR/dark*$i*.fits` processed_slitflat:`ls $CALDIR/flat*$j*SLIT*.fits` processed_slit:`ls $CALDIR/arc*$j*SLIT*.fits` processed_polyfit:`ls $CALDIR/*$i*$j*xmod*.fits`
+    done
+done
 
 
-# echo 'Doing red now'
-# typewalk --types GHOST_BIAS GHOST_RED --dir biases/ -o bias.list
-# reduce @bias.list
-# typewalk --types GHOST_DARK GHOST_RED --dir darks/ -o dark.list
-# reduce @dark.list --override_cal processed_bias:`ls calibrations/storedcals/bias_*_red_*.fits`
-
-# #FLATS
-# typewalk --types GHOST_FLAT GHOST_RED GHOST_HIGH --dir flats/high/ -o flat.list
-# reduce @flat.list --override_cal processed_bias:`ls calibrations/storedcals/bias*red_*.fits` processed_dark:`ls calibrations/storedcals/dark*red*.fits`
-# typewalk --types GHOST_FLAT GHOST_RED GHOST_STD --dir flats/std/ -o flat.list
-# reduce @flat.list --override_cal processed_bias:`ls calibrations/storedcals/bias*red_*.fits` processed_dark:`ls calibrations/storedcals/dark*red*.fits`
-
-#ARCS
-typewalk --types GHOST_ARC GHOST_RED GHOST_HIGH --dir arcs/ -o arc.list
-reduce @arc.list --override_cal processed_bias:`ls calibrations/storedcals/bias*red_*.fits` processed_dark:`ls calibrations/storedcals/dark*red*.fits` processed_slitflat:`ls calibrations/storedcals/flat*high*SLIT*.fits` processed_slit:`ls calibrations/storedcals/arc*high*SLIT*.fits` processed_polyfit:`ls calibrations/storedcals/*red*high*xmod*.fits`
-typewalk --types GHOST_ARC GHOST_RED GHOST_STD --dir arcs/ -o arc.list
-reduce @arc.list --override_cal processed_bias:`ls calibrations/storedcals/bias*red_*.fits` processed_dark:`ls calibrations/storedcals/dark*red*.fits` processed_slitflat:`ls calibrations/storedcals/flat*std*SLIT*.fits` processed_slit:`ls calibrations/storedcals/arc*std*SLIT*.fits`  processed_polyfit:`ls calibrations/storedcals/*red*std*xmod*.fits`
 
 
 
