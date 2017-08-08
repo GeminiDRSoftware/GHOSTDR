a63292d29d79576fb15aa58cb116d192801c120c
Lance Luvaul <lance.luvaul@anu.edu.au>
2017-08-08 13:14:03 +1000

    update doco to reflect reduce_all, and update reduce_all to reduce object frames

--- utils/reduce_all.sh
+++ utils/reduce_all.sh
@@ -3,7 +3,7 @@
 #Script to reduce all GHOST data using the recipe system.
 #This must be ran from within where the GHOST data are kept, with default variables indicating where the various types of files are
 
-# Start by setting up all the locations for the files. Change this for each case. 
+# Start by setting up all the locations for the files. Change this for each case.
 COREDIR=$PWD'/original'
 
 CALDIR=$PWD'/calibrations/storedcals'
@@ -16,53 +16,86 @@ FLATDIR=$COREDIR
 ARCDIR=$COREDIR
 OBJDIR=$COREDIR
 
-#This line is for cleaning up your directory of all the stuff the reduction creates. 
+#This line is for cleaning up your directory of all the stuff the reduction creates.
 #rm *stack* *forStack* adc* *.log *.list tmp* *_dark.fits *_bias.fits GHOST* *_arc.fits *_flat.fits
 
-   
+
 echo 'Doing slits now'
+
 typewalk --types GHOST_SLITV_BIAS --dir $BIASDIR/ -o bias.list
 reduce @bias.list
+
 typewalk --types GHOST_SLITV_DARK --dir $DARKDIR/ -o dark.list
 reduce @dark.list --override_cal processed_bias:`ls $CALDIR/bias*SLIT*.fits`
 
-for j in high std
-do
-    CAPMODE=`echo $j | tr '[:lower:]' '[:upper:]'`
+for mode in high std; do
+    CAPMODE=`echo $mode | tr '[:lower:]' '[:upper:]'`
+
     typewalk --types GHOST_SLITV_FLAT GHOST_$CAPMODE --dir $FLATDIR/ -o flat.list
-    reduce @flat.list --override_cal processed_bias:`ls $CALDIR/bias*SLIT*.fits` processed_dark:`ls $CALDIR/dark*SLIT*.fits`
+    reduce @flat.list --override_cal \
+        processed_bias:`ls $CALDIR/bias*SLIT*.fits` \
+        processed_dark:`ls $CALDIR/dark*SLIT*.fits`
+
     typewalk --types GHOST_SLITV_ARC GHOST_$CAPMODE --dir $ARCDIR/ -o arc.list
-    reduce @arc.list --override_cal processed_bias:`ls $CALDIR/bias*SLIT*.fits` processed_dark:`ls $CALDIR/dark*SLIT*.fits` processed_slitflat:`ls $CALDIR/flat*$j*SLIT*.fits`
-    #The next 2 lines are supposed to reduce any target data, but currently
-    #the recipe system requires each data frame to be reduced individually.
-    #typewalk --types GHOST_SLITV_IMAGE GHOST_$CAPMODE --dir $OBJDIR/ -o object.list
-    #reduce @object.list --override_cal processed_bias:`ls $CALDIR/bias*SLIT*.fits` processed_dark:`ls $CALDIR/dark*SLIT*.fits` processed_slitflat:`ls $CALDIR/flat*$j*SLIT*.fits`
+    reduce @arc.list --override_cal \
+        processed_bias:`ls $CALDIR/bias*SLIT*.fits` \
+        processed_dark:`ls $CALDIR/dark*SLIT*.fits` \
+        processed_slitflat:`ls $CALDIR/flat*$mode*SLIT*.fits`
+
+    while read object <&3; do
+        reduce $object --override_cal \
+            processed_bias:`ls $CALDIR/bias*SLIT*.fits` \
+            processed_dark:`ls $CALDIR/dark*SLIT*.fits` \
+            processed_slitflat:`ls $CALDIR/flat*$mode*SLIT*.fits`
+    done 3< <(
+        typewalk --types GHOST_SLITV_IMAGE GHOST_$CAPMODE --dir $OBJDIR/ --filemask 'obj.*\.(fits|FITS)' \
+        | grep '^ ' \
+        | awk '{print $1}'
+    )
 done
 
 echo 'Now the spectrograph data'
-for i in red blue
-do
-    echo "Doing $i images now"
-    CAPCAM=`echo $i | tr '[:lower:]' '[:upper:]'`
-    typewalk --types GHOST_BIAS GHOST_$CAPCAM --dir "$BIASDIR" -o bias.list
+
+for cam in red blue; do
+    echo "Doing $cam images now"
+    CAPCAM=`echo $cam | tr '[:lower:]' '[:upper:]'`
+
+    typewalk --types GHOST_BIAS GHOST_$CAPCAM --dir $BIASDIR/ --filemask '.*1x1.*\.(fits|FITS)' -o bias.list
     reduce @bias.list
-    typewalk --types GHOST_DARK GHOST_$CAPCAM --dir $DARKDIR -o dark.list
-    reduce @dark.list --override_cal processed_bias:`ls $CALDIR/bias*$i*.fits`
-    for j in high std
-    do
-	CAPMODE=`echo $j | tr '[:lower:]' '[:upper:]'`
-	typewalk --types GHOST_FLAT GHOST_$CAPCAM GHOST_$CAPMODE --dir $FLATDIR/ -o flat.list
-	reduce @flat.list --override_cal processed_bias:`ls $CALDIR/bias*$i*.fits` processed_dark:`ls $CALDIR/dark*$i*.fits`
-	typewalk --types GHOST_ARC GHOST_$CAPCAM GHOST_$CAPMODE --dir $ARCDIR/ -o arc.list
-	reduce @arc.list --override_cal processed_bias:`ls $CALDIR/bias*$i*.fits` processed_dark:`ls $CALDIR/dark*$i*.fits` processed_slitflat:`ls $CALDIR/flat*$j*SLIT*.fits` processed_slit:`ls $CALDIR/arc*$j*SLIT*.fits` processed_xmod:`ls $CALDIR/*$i*$j*xmod*.fits`
-	#The next 2 lines are supposed to reduce any target data, but currently
-	#the recipe system requires each data frame to be reduced individually.
-	#typewalk --types GHOST_OBJECT GHOST_$CAPCAM GHOST_$CAPMODE --dir $OBJDIR/ -o object.list
-	#reduce @object.list --override_cal processed_bias:`ls $CALDIR/bias*$i*.fits` processed_dark:`ls $CALDIR/dark*$i*.fits` processed_slitflat:`ls $CALDIR/flat*$j*SLIT*.fits` processed_slit:`ls $CALDIR/obj*$j*SLIT*.fits` processed_xmod:`ls $CALDIR/*$i*$j*xmod*.fits` processed_flat:`ls $CALDIR/flat*$j*$i*.fits`
+
+    typewalk --types GHOST_DARK GHOST_$CAPCAM --dir $DARKDIR/ -o dark.list
+    reduce @dark.list --override_cal processed_bias:`ls $CALDIR/bias*$cam*.fits`
+
+    for mode in high std; do
+        CAPMODE=`echo $mode | tr '[:lower:]' '[:upper:]'`
+
+        typewalk --types GHOST_FLAT GHOST_$CAPCAM GHOST_$CAPMODE --dir $FLATDIR/ -o flat.list
+        reduce @flat.list --override_cal \
+            processed_bias:`ls $CALDIR/bias*$cam*.fits` \
+            processed_dark:`ls $CALDIR/dark*$cam*.fits` \
+			processed_slitflat:`ls $CALDIR/flat*$mode*SLIT*.fits` \
+            processed_xmod:`ls $CALDIR/*$cam*$mode*xmod*.fits`
+
+        typewalk --types GHOST_ARC GHOST_$CAPCAM GHOST_$CAPMODE --dir $ARCDIR/ -o arc.list
+        reduce @arc.list --override_cal \
+            processed_bias:`ls $CALDIR/bias*$cam*.fits` \
+            processed_dark:`ls $CALDIR/dark*$cam*.fits` \
+            processed_slitflat:`ls $CALDIR/flat*$mode*SLIT*.fits` \
+            processed_slit:`ls $CALDIR/arc*$mode*SLIT*.fits` \
+            processed_xmod:`ls $CALDIR/*$cam*$mode*xmod*.fits`
+
+        while read object <&3; do
+            reduce $object --override_cal \
+                processed_bias:`ls $CALDIR/bias*$cam*.fits` \
+                processed_dark:`ls $CALDIR/dark*$cam*.fits` \
+                processed_slitflat:`ls $CALDIR/flat*$mode*SLIT*.fits` \
+                processed_slit:`ls $CALDIR/obj*$mode*SLIT*.fits` \
+                processed_xmod:`ls $CALDIR/*$cam*$mode*xmod*.fits` \
+                processed_flat:`ls $CALDIR/flat*$mode*$cam*.fits`
+        done 3< <(
+            typewalk --types GHOST_OBJECT GHOST_$CAPCAM GHOST_$CAPMODE --dir $OBJDIR/ --filemask '.*1x1.*\.(fits|FITS)' \
+            | grep '^ ' \
+            | awk '{print $1}'
+        )
     done
 done
-
-
-
-
-
