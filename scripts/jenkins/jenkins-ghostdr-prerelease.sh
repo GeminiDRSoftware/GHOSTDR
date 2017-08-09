b95743b3da0b2a0c4fcd0b52cfd9eec4458240f2
Jon Nielsen <jon.nielsen@anu.edu.au>
2017-08-09 14:40:15 +1000

    Debugging added to jenkins prerelease script

--- scripts/jenkins/jenkins-ghostdr-prerelease.sh
+++ scripts/jenkins/jenkins-ghostdr-prerelease.sh
@@ -7,13 +7,18 @@
 # Exit if any command returns an error
 set -e
 
+echo "This is the jenkins pre-release script"
+echo "RELEASE_VERSION = ${RELEASE_VERSION}"
+echo "RELEASE_REVISION = ${RELEASE_REVISION}"
+echo "RELEASE_DESCRIPTION = ${RELEASE_DESCRIPTION}"
+
 # Prepend the details of the new release to the file that contains the list of releases
 RELEASE_STR="${RELEASE_VERSION}
   ${RELEASE_DESCRIPTION}
 
 "
 # Only mess with the repository from one host, not all the build hosts.  Might as well be the master.
-if [ ${NODE_NAME} == "master" ]; then
+if [ "${NODE_NAME}" == "master" ]; then
   # Start in the repository
   cd ${WORKSPACE}/hg
 
