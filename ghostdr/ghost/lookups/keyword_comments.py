72e18ba14fdb46802bc022ad07eb17d05ef6a523
= <marc.white@anu.edu.au>
2018-02-20 13:33:39 +1100

    Added code comments to GHOST keyword_comments

--- ghostdr/ghost/lookups/keyword_comments.py
+++ ghostdr/ghost/lookups/keyword_comments.py
@@ -1,4 +1,6 @@
 keyword_comments = {
+    # Dictionary keys are header keywords that are commonly updated
+    # Dictionary values are standard comments for each keyword
     "FLATBPM": "Flat used to propagate BPM",
     "ARCIM_B": "'Before' arc image",
     "ARCIM_A": "'After' arc image",
