79ebe854a77dcf2b65141082d31baec1276c8730
= <marc.white@anu.edu.au>
2018-04-03 18:59:09 +1000

    Test outputs now appear in the temporary directory created by pytest

--- ghostdr/ghost/recipes/test/test_overscanCorrect.py
+++ ghostdr/ghost/recipes/test/test_overscanCorrect.py
@@ -6,6 +6,7 @@ import shutil
 
 from recipe_system.reduction.coreReduce import Reduce
 from gempy.utils import logutils
+import astrodata
 
 # TESTS TO RUN #
 
@@ -20,6 +21,8 @@ class TestOverscanSubtractClass(object):
         rawfilename = 'bias_1_1x1_blue.fits'
         # Copy the raw data file into here
         tmpsubdir = tmpdir_factory.mktemp('fits')
+        # Make sure we're working inside the temp dir
+        os.chdir(tmpsubdir.dirname)
         shutil.copy(
             os.path.join(os.path.dirname(os.path.abspath(__file__)),
                          'testdata',
@@ -31,32 +34,47 @@ class TestOverscanSubtractClass(object):
         reduce = Reduce()
         reduce.drpkg = 'ghostdr'
         reduce.files = [rawfile, ]
-        reduce.mode = 'test'
+        reduce.mode = ['test', ]
         reduce.recipe = 'recipes_BIAS.recipeBiasRemoveOverscan'
         reduce.recipename = 'recipes_BIAS.recipeBiasRemoveOverscan'
         reduce.logfile = os.path.join(tmpsubdir.dirname,
                                       'reduce_overscancorrect.log')
         reduce.logmode = 'quiet'
+        reduce.suffix = '_testoutput'
         logutils.config(file_name=reduce.logfile, mode=reduce.logmode)
         reduce.runr()
 
+        corrfilename = rawfilename.split('_')[0] + reduce.suffix + '.fits'
+        corrfile = os.path.join(tmpsubdir.dirname, corrfilename)
+
         # Return filenames of raw, subtracted files
-        return rawfile, 'blah'
+        return rawfile, corrfile
 
     def test_overscan_headerkw(self, do_overscan_subtract):
         rawfile, corrfile = do_overscan_subtract
-        # print(rawfile)
-        # print(corrfile)
-        assert True
+        corrad = astrodata.open(corrfile)
+        assert corrad.phu.get('SUBOVER') and corrad.phu.get('TRIMOVER')
 
     def test_overscan_mean(self, do_overscan_subtract):
         rawfile, corrfile = do_overscan_subtract
-        assert 1 == 1
+        rawad = astrodata.open(rawfile)
+        corrad = astrodata.open(corrfile)
+        for ext in rawad:
+            pass
+        assert True
 
     def test_overscan_std(self, do_overscan_subtract):
         rawfile, corrfile = do_overscan_subtract
-        assert 1 == 1
+        rawad = astrodata.open(rawfile)
+        corrad = astrodata.open(corrfile)
+        for ext in rawad:
+            pass
+        assert True
 
     def test_overscan_shape(self, do_overscan_subtract):
         rawfile, corrfile = do_overscan_subtract
-        assert 1 == 1
+        rawad = astrodata.open(rawfile)
+        corrad = astrodata.open(corrfile)
+        for ext in rawad:
+            pass
+        assert True
