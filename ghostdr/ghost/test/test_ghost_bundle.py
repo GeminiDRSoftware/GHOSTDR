4982cf0b7df8be5d87336246e6d7939d8ac520eb
= <marc.white@anu.edu.au>
2018-08-02 16:56:59 +1000

    Full splitBundle testing on pseudo-data

--- ghostdr/ghost/test/test_ghost_bundle.py
+++ ghostdr/ghost/test/test_ghost_bundle.py
@@ -52,27 +52,49 @@ class TestGhostBundle:
         for key, value in BUNDLE_STRUCTURE.items():
             for i in range(value):
                 hdu = fits.ImageHDU(data=np.zeros((10, 10, )), name='SCI')
-                hdu.header['CAMERA'] = key[0]
-                hdu.header['EXPID'] = key[1]
+                hdu.header.set('CAMERA', key[0])
+                hdu.header.set('CCDNAME', key[1])  # Sends keyword to split files
+                hdu.header.set('EXPID', key[1])
+                hdu.header.set('CCDSUM', '1 1')
                 hdus.append(hdu)
 
         # Create AstroData
         ad = astrodata.create(phu, hdus)
-
-        return ad, tmpsubdir
-
-    def test_splitBundle(self, create_bundle):
-        """
-        Checks to make:
-
-        - Count number of extracted files
-        - Ensure function itself returns empty list
-        """
-        dummy_bundle, tmpsubdir = create_bundle
+        ad.filename = rawfilename
 
         # Do things in the tmpdir
         os.chdir(tmpsubdir.dirname)
-        p = GHOSTBundle([dummy_bundle, ])
-        output = p.splitBundle(adinputs=[dummy_bundle, ])
-        print('Files in tmpdir: {}'.format(glob.glob(tmpsubdir.dirname)))
-        assert False
+        p = GHOSTBundle([ad, ])
+        bundle_output = p.splitBundle(adinputs=[ad, ])
+
+        return ad, tmpsubdir, bundle_output
+
+    def test_splitBundle_output(self, create_bundle):
+        """
+        Check that splitBundle outputs the empty list
+        """
+        dummy_bundle, tmpsubdir, bundle_output = create_bundle
+        assert bundle_output == []
+
+    def test_splitBundle_count(self, create_bundle):
+        """
+        Check that the right number of files have been extracted from the
+        bundle
+        """
+        dummy_bundle, tmpsubdir, bundle_output = create_bundle
+        file_list = glob.glob(os.path.join(tmpsubdir.dirname, '*.fits'))
+        assert len(file_list) == len(BUNDLE_STRUCTURE)
+
+    def test_splitBundle_count(self, create_bundle):
+        """
+        Check the structure of the output files
+        """
+        dummy_bundle, tmpsubdir, bundle_output = create_bundle
+        file_list = glob.glob(os.path.join(tmpsubdir.dirname, '*.fits'))
+        # Check files written to disk
+        for o in file_list:
+            ad = astrodata.open(o)
+            assert len(ad) == BUNDLE_STRUCTURE[(ad.phu.get('CAMERA'),
+                                                ad.phu.get('CCDNAME'))]
+
+
