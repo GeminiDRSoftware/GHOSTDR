00a1ffacbc05d27f0ff87d533061bb00347e1239
= <marc.white@anu.edu.au>
2019-10-24 11:27:02 +1100

    Made some minor import updates for Python 3 compatibility

--- ghostdr/ghost/polyfit/__init__.py
+++ ghostdr/ghost/polyfit/__init__.py
@@ -9,8 +9,8 @@ __author__ = "Michael Ireland <michael.ireland@anu.edu.au> and " \
              "Joao Bento <joao.bento@anu.edu.au>"
 __version__ = "1.0"
 
-from ghost import GhostArm
-from slitview import SlitView
-from extract import Extractor
+from .ghost import GhostArm
+from .slitview import SlitView
+from .extract import Extractor
 
 
