f29d0c47f845db57d7e959bc3ef9ff9d8d689114
Jon Nielsen <jon.nielsen@anu.edu.au>
2022-03-24 14:37:53 +1100

    Minor fix for better pylint score

--- ghost_instruments/__init__.py
+++ ghost_instruments/__init__.py
@@ -1,2 +1,7 @@
+"""
+The ghost_instruments package contains descriptors, Astrodata tags
+and other configuration specific to the instrument.
+"""
+
 # Allows you to just "import ghost_instruments"
 from . import ghost
