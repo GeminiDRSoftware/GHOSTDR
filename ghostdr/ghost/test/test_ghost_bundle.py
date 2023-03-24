210b70f36bb00e9aa28033e9100dac65e0c1e3bb
chris-simpson <csimpson@gemini.edu>
2023-03-24 09:33:29 -1000

    rewrite splitBundle() test

--- ghostdr/ghost/test/test_ghost_bundle.py
+++ ghostdr/ghost/test/test_ghost_bundle.py
@@ -9,42 +9,32 @@ import astrodata, ghost_instruments
 from astrodata.testing import download_from_archive
 from gempy.utils import logutils
 
-from astropy.io import fits
-
 from ghostdr.ghost.primitives_ghost_bundle import GHOSTBundle
 
 
 @pytest.mark.ghostbundle
-class TestGhostBundle:
+def test_split_bundle(change_working_dir):
     """
-    Suite of tests for the functions in the primitives_ghost_bundle module
+    S20230214S0025 has 1 blue, 3 red, and 5 slit images
     """
+    with change_working_dir():
+        ad = astrodata.open(download_from_archive("S20230214S0025.fits"))
+    p = GHOSTBundle([ad])
+    p.splitBundle()
 
-    @pytest.fixture(scope='class')
-    def create_bundle(self, change_working_dir):
-        """
-        Download and return it and the result of splitting it
-        """
-        with change_working_dir():
-            ad = astrodata.open(download_from_archive("S20230214S0025.fits"))
-        p = GHOSTBundle([ad, ])
-        bundle_output = p.splitBundle(adinputs=[ad])
-        return ad, bundle_output
+    assert len(p.streams['main']) == 5
 
-    def test_splitBundle_count(self, create_bundle):
-        """
-        Check that the right number of files have been extracted from the
-        bundle
-        """
-        dummy_bundle, bundle_output = create_bundle
-        assert len(bundle_output) == 4
+    blue_files = p.selectFromInputs(tags="BLUE", outstream='blue')
+    red_files = p.selectFromInputs(tags="RED", outstream='red')
+    slit_files = p.selectFromInputs(tags="SLITV")
 
-    def test_splitBundle_structure(self, create_bundle):
-        """
-        Check the structure of the output files
-        """
-        #dummy_bundle, bundle_output = create_bundle
-        # Check files written to disk
-        #for ad in bundle_output:
-        #    assert len(ad) == BUNDLE_STRUCTURE[(ad[0].hdr.get('CAMERA'),
-        #                                        ad[0].hdr.get('CCDNAME'))]
+    assert len(blue_files) == 1
+    assert len(red_files) == 3
+    assert len(slit_files) == 1
+
+    for ad in blue_files + red_files:
+        assert len(ad) == 4
+    assert len(slit_files[0]) == 5
+
+    sciexp = slit_files[0].SCIEXP
+    assert len(sciexp) == 4
