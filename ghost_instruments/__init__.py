bd6fcb05b7e186fda41a60dc790043de3f467c24
Kathleen Labrie <kathleen.labrie.phd@gmail.com>
2023-05-12 10:06:41 -1000

    update version number following a fix for HR mode.

--- ghost_instruments/__init__.py
+++ ghost_instruments/__init__.py
@@ -3,7 +3,7 @@ The ghost_instruments package contains descriptors, Astrodata tags
 and other configuration specific to the instrument.
 """
 
-__version__ = '0.9.1'
+__version__ = '0.9.2'
 
 # Allows you to just "import ghost_instruments"
 from . import ghost
