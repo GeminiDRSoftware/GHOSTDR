d0c0d7c0053be017bde090f1f4418fffa988ea27
chris-simpson <csimpson@gemini.edu>
2023-07-19 15:55:54 -1000

    update v0.9.2 -> v0.9.3

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ for p in SIMPACKAGES:
 
 setup(
     name='ghostdr',
-    version='0.9.2',
+    version='0.9.3',
     description='GHOST Data Reduction',
     author="ANU & Gemini Observatory",
     url="https://ghost-drtutorial.readthedocs.io/en/release-3.0.x/index.html",
