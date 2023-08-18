8020562faefccb09a4c625bfad432f110343920c
chris-simpson <csimpson@gemini.edu>
2023-08-17 17:35:44 -1000

    set version to 1.0.0

--- ghost_instruments/__init__.py
+++ ghost_instruments/__init__.py
@@ -3,7 +3,7 @@ The ghost_instruments package contains descriptors, Astrodata tags
 and other configuration specific to the instrument.
 """
 
-__version__ = '0.9.4'
+__version__ = '1.0.0'
 
 # Allows you to just "import ghost_instruments"
 from . import ghost
--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ for p in SIMPACKAGES:
 
 setup(
     name='ghostdr',
-    version='0.9.4',
+    version='1.0.0',
     description='GHOST Data Reduction',
     author="ANU & Gemini Observatory",
     url="https://ghost-drtutorial.readthedocs.io/en/release-3.0.x/index.html",
